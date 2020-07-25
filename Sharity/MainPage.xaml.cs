691b4c38fae2b51f750362d27809850be417e0f8
Mohd Aftab Alam Ansari <68464806+Aftab4772@users.noreply.github.com>
2020-07-25 16:28:23 +0530

    Initial commit

---
+++ Sharity/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Sharity
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        
+        private async void Donate_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Important!!!", "App Development is progress.", "Ok");
+        }
+
+        private async void Recive_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Important!!!", "App Development is progress.", "Ok");
+        }
+    }
+}
